a0d7474a4aeb2f0e6979ef6448c87e947b1713fb
Gabriel Aragao <gabrielaragaofds@gmail.com>
2019-06-23 12:55:32 -0300

    vs 0.0.3 shield

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,27 +4,45 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    private float direcX, speed;
-    public static Vector3 cameraP;
-    private Vector3 comeco;
-    private Vector2 Mouse;
-    bool Eativado;
+
+    [SerializeField] private GameObject[] Escudos = new GameObject[3];
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject mouse;
+    private GameObject clone;
+
+    public static Vector3 cameraP;
+    
+    private float direcX, speed;
+    private int shield;
+    private Vector3 comeco;
+    private Vector2 Mouse;
+    bool Eativado ,EAtivo;
+    
+    
     void movimento()
     {
 
         if (!Eativado)
         {
+            direcX = Input.GetAxis("Horizontal");
             transform.Translate(Vector3.right * Time.deltaTime * direcX * speed);
         }
-        if (Input.GetKeyDown(KeyCode.Mouse0) && direcX == 0)
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Eativado = true;
             animator.SetBool("Eativado", true);
-        }
+        } 
         if (Eativado)
         {
+            if (!EAtivo)
+            {
+               // clone = Instantiate(Escudos[shield], transform.position , Quaternion.identity);
+                EAtivo = true;
+            }
+
+            direcX = 0;
+            #region animacaoEscudoEsquerdo
+            //ESuperiorEsquerdo
             if (Mouse.y >= 1.5f && Mouse.x <= -1.5)
             {
                 animator.SetBool("EcimaEsquerda", false);
@@ -34,6 +52,7 @@ public class PlayerBehaviour : MonoBehaviour
                 animator.SetBool("EmeioEsquerda", false);
                 animator.SetBool("EsuperiorEsquerda", true);
             }
+            //EMeioEsquerda
             if (Mouse.x <= 0f && Mouse.y <= 1.5f)
             {
                 animator.SetBool("EcimaEsquerda", false);
@@ -44,6 +63,7 @@ public class PlayerBehaviour : MonoBehaviour
                 animator.SetBool("EmeioEsquerda", true);
                 
             }
+            //ECimaEsquerda
             if (Mouse.y >= 2.5 && Mouse.x <= 0f && Mouse.x >= -1.5f)
             {
                 animator.SetBool("EsuperiorDireito", false);
@@ -54,17 +74,24 @@ public class PlayerBehaviour : MonoBehaviour
                 animator.SetBool("EcimaEsquerda", true);
 
             }
-             if (Mouse.y >= 2.5 && Mouse.x >=0f && Mouse.x <=1.5f)
+            #endregion
+            #region animacaoEscudoDireito
+            //ECimaDireita           
+            if (Mouse.y >= 2.5 && Mouse.x >=0f && Mouse.x <=1.5f)
              {
+                //instancia o escudo
+                clone.transform.position = transform.position + new Vector3(-0.5f,1f);
+              //  clone.transform.Rotate(0, 0, 90);
                 animator.SetBool("EcimaEsquerda", false);
                 animator.SetBool("EsuperiorDireito", false);
                 animator.SetBool("EmeioDireito", false);
                 animator.SetBool("EmeioEsquerda", false);
                 animator.SetBool("EsuperiorEsquerda", false);
                 animator.SetBool("Ecima", true);
+               
              }
-           
-             if (Mouse.y >=1.5f && Mouse.x >=1.5)
+            //ESuperiorDireito
+            if (Mouse.y >=1.5f && Mouse.x >=1.5)
              {
                 animator.SetBool("EcimaEsquerda", false);
                 animator.SetBool("EmeioDireito", false);
@@ -72,9 +99,9 @@ public class PlayerBehaviour : MonoBehaviour
                 animator.SetBool("EmeioEsquerda", false);
                 animator.SetBool("EsuperiorEsquerda", false);
                 animator.SetBool("EsuperiorDireito", true);
-             } 
-                
-             if (Mouse.x >= 0f && Mouse.y <=1.5f )
+             }
+            //EMeioDireito   
+            if (Mouse.x >= 0f && Mouse.y <=1.5f )
              {
                 animator.SetBool("EcimaEsquerda", false);
                 animator.SetBool("EsuperiorDireito", false);
@@ -83,11 +110,15 @@ public class PlayerBehaviour : MonoBehaviour
                 animator.SetBool("EsuperiorEsquerda", false);
                 animator.SetBool("EmeioDireito", true);
              }
-             
-             if (Input.GetKeyUp(KeyCode.Mouse0))
+            #endregion
+            //Finalizada a Animação
+            if (Input.GetKeyUp(KeyCode.Mouse0))
              {
+                 speed = 8;
+                 Destroy(this.clone);
                  Eativado = false;
-                animator.SetBool("Eativado", false);
+                 EAtivo = false;
+                 animator.SetBool("Eativado", false);
                  animator.SetBool("EcimaEsquerda", false);
                  animator.SetBool("EsuperiorDireito", false);
                  animator.SetBool("EmeioDireito", false);
@@ -101,10 +132,13 @@ public class PlayerBehaviour : MonoBehaviour
         // Start is called before the first frame update
         void Start()
         {
+        // por enquanto
+            shield = 0;
             cameraP = transform.position + new Vector3(0, 0, -10);
             speed = 8;
             comeco = new Vector3(0, 0);
             Eativado = false;
+            EAtivo = false;
         }
 
         // Update is called once per frame
@@ -117,7 +151,7 @@ public class PlayerBehaviour : MonoBehaviour
         print(Mouse);
             cameraP = transform.position + new Vector3(0, 0, -10);
             animator.SetFloat("direcX", direcX);
-            direcX = Input.GetAxis("Horizontal");
+           
             movimento();
         }
     }

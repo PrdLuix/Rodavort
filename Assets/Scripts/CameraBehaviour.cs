5c7c329a9cef9deacef93c557d2504248ad05ed8
Gabriel Aragão <gabrielaragaofds@gmail.com>
2019-06-28 17:01:04 -0300

    vs 0.0.6 cameraPosition

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
+    private float xmin,xmax,cameraZ;
    
     void Start()
     {
         transform.position = PlayerBehaviour.cameraP;
+        xmin = -28;
+        cameraZ= -10;
+        xmax = 15;
     }
 
     void Update()
     {
-        transform.position = PlayerBehaviour.cameraP;
+         #region LimiteCasa
+        if (PlayerBehaviour.cameraP.x < xmin)
+        {
+           transform.position = new Vector3(xmin,transform.position.y,cameraZ);
+        }
+        else if (PlayerBehaviour.cameraP.x >xmax)
+        {
+           transform.position = new Vector3(xmax,transform.position.y,cameraZ);
+        }
+        else
+        {
+             transform.position =new Vector3(PlayerBehaviour.cameraP.x,PlayerBehaviour.cameraP.y,cameraZ);
+        }
+        #endregion
+       
     }
 }

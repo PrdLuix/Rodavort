3da510a4dcbeef5c1d0f4bf0d6c3e27c3a6b5612
Aluno <aluno@naverecife.local>
2019-07-18 12:51:34 -0300

    vs 1.8 chuva pronta

---
+++ Assets/RainBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainBehaviour : MonoBehaviour
+{
+    float speed;
+    void Update()
+    {
+        speed = 5 *Time.deltaTime;
+        transform.position += new Vector3(speed,-speed);
+    }
+}
---
+++ Assets/SpawnerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBehaviour : MonoBehaviour
+{
+    [SerializeField] GameObject rain;
+    void Start()
+    {
+      InvokeRepeating("Rain", 0, 0.3f);
+    }
+    void Rain()
+    {
+        Destroy(Instantiate(rain,transform.position,Quaternion.identity),2);
+    }
+
+}
